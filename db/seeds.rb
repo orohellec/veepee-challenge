4eb38d908c967675db6ec2b4188d829cd2212f83
Olivier <orohellec44@gmail.com>
2020-02-03 01:47:05 +0100

    add require faker in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# require 'faker'
+
+require 'faker'
 
 AdminUser.delete_all
 Product.delete_all
